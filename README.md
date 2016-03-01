760627a10739500462eedf02f92cb714a1701341
andyafter <andyafter@gmail.com>
2016-03-01 17:25:53 +0800

    added database model

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This repository is written with scrapy.
 
-- pip install -r requirements.txt
 - createdb crawler(if you are using postgresql)
+- pip install -r requirements.txt
 
-Of course if you want to use Postgresql you can also run **postsql.sh** to start database in your local machine(just for my own convenience).
+Of course if you want to use Postgresql you can also run **postsql.sh** to start database in your local machine(just for my own convenience). I used SQLAlchemy so if anyone wants to change the database you will have to do some modifications based on that. If nobody wants to do it, you can leave it to me.
--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text, SmallInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.engine.url import URL
+from sqlalchemy import UniqueConstraint
 
 import settings
 
@@ -24,8 +25,37 @@ def create_deals_table(engine):
 
 
 class ItemInfo(DeclarativeBase):
-    __tablename__ = "hahaha"
+    __tablename__ = "product"
+    # here sqlalchemy have settings for mysql to set integer as 10
+    # I'm not gonna put too much limitation on the database table columns
     id = Column(Integer, primary_key=True)
+    pid = Column('pid', String(45), unique=True)
+    tag_source = Column('tag_source', Text, nullable = False)
+    # unless strongly urged I'm not gonna write any nullables
+    tas_id = Column('tags_id', Text)
+    update_time = Column('update_time', DateTime, nullable = False, default=datetime.datetime.utcnow)
+    category = Column('category', Integer(unsigned=True), nullable = False, default = 0)
+    sub_category = Column('sub_category', Integer(unsigned=True), nullable = False, default = 0)
+    brand = Column('brand', Integer(unsigned=True), nullable = False, default = 0)
+    detail_images = Column('detail_images', String(4000), nullable = False)
+    thumb_images = Column('thumb_images', String(500), nullable = True)
+    suitable_images = Column('suitable_images', String(500), nullable = False)
+    white_suitable = Column('white_suitable', String(500), nullable = False)
+    tag_status = Column('tag_status', SmallInteger)
+    visenze_result = Column('visenze_result', Text)
+    merchant = Column('merchant', Integer, nullable=False, default=0)
+    koutu = Column('koutu', String(500))
+    shop_url = Column('shop_url', String(500))
+    brand_en = Column('brand_en', String(45))
+    merchant_en = Column('merchant_en', String(45))
+    price = Column('price', Integer)
+    discount_price = Column('discount_price', Integer)
+    discount_percent = Column('discount_percent', Integer)
+    buy_size = Column('buy_size', String(500), nullable = False, default='[]')
+    buy_color = Column('buy_color', String(500), nullable = False, default='[]')
+    stock_info = Column('stock_info', String(1000), nullable=False)
+    status = Column('status', String(1000), nullable=False)
+
 
 
 class Test(DeclarativeBase):
